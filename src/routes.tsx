a21d3590e04498b0cfa16f6b7d7c7ad78bf17ddb
bllackdev <rapha_rmm@hotmail.com>
2020-08-04 13:17:13 -0300

    add TeachersList e TeacherForm

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="" />
+    </BrowserRouter>
+  )
+}
