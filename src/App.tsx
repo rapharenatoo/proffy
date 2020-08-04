d8f69b53b1d1322b420242b6479121d0ccb6a58d
bllackdev <rapha_rmm@hotmail.com>
2020-08-03 23:49:30 -0300

    Estrutura Inicial e add Landing Page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing/index';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
