3290c7ae104b99f8cfe96181f3e53ae64aedc7a8
bllackdev <rapha_rmm@hotmail.com>
2020-08-08 19:56:55 -0300

    add Componente Input

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -8,10 +8,7 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+
 
           <div className="input-block">
             <label htmlFor="week_day">Dia da Semana</label>
